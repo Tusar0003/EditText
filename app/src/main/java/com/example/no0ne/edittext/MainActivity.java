b8062f29057580f87bb09981082199c11edbc3fe
Tusar0003 <faisalahmedtusar@gmail.com>
2018-08-21 00:26:37 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/no0ne/edittext/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.no0ne.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setEditText();
+    }
+
+    public void setEditText() {
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String changedText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, changedText, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
